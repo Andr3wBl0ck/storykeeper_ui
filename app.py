008b1fb3a45c9fc7ffa17160e51285b181e66154
AB <andrew.block09@gmail.com>
2022-03-12 14:53:55 -0800

    Added inventory, cart and orderhistory page. Added flask routing and base templates.

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def render_dashboard():
     return render_template("dashboard.html")
+
+@app.route("/cart")
+def render_cart():
+    return render_template("cart.html")
+
+@app.route("/inventory")
+def render_inventory():
+    return render_template("inventory.html")
+
+@app.route("/orderhistory")
+def render_orderhistory():
+    return render_template("orderhistory.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
